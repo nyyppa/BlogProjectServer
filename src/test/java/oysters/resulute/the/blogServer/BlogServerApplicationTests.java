05bf54e7e67c5eb49e119746ebadfe8fda9fcde8
nyyppa <atte.yliverronen@gmail.com>
2020-03-07 11:52:27 +0200

    Initial commit

---
+++ src/test/java/oysters/resulute/the/blogServer/BlogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package oysters.resulute.the.blogServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
