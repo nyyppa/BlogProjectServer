a2ba1eb7dc426eef5f774ec9970e66141deec771
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-04-27 18:11:24 +0300

    react files

---
+++ src/main/resources/static/precache-manifest.f5f8c29dcbdfd80906d834d8d557c761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c91235c28bbcd3060c2b9ae07b5c5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e81018f7e5736f522af",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "178e4e298659f48d301c",
+    "url": "./static/js/2.19503a89.chunk.js"
+  },
+  {
+    "revision": "c1f316ce43a1ab270653bf06e61be3e0",
+    "url": "./static/js/2.19503a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e81018f7e5736f522af",
+    "url": "./static/js/main.7ed15350.chunk.js"
+  },
+  {
+    "revision": "a6621537bb9358b27492",
+    "url": "./static/js/runtime-main.eccd4b2e.js"
+  }
+]);
