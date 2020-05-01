a974b8ebfb62c0f3d3c58c58f898eef49d51f8f0
nyyppa <atte.yliverronen@gmail.com>
2020-05-01 10:45:42 +0300

    initial basic-auth

--- src/main/java/oysters/resulute/the/blogServer/WebConfig.java
+++ src/main/java/oysters/resulute/the/blogServer/WebConfig.java
@@ -3,6 +3,7 @@ package oysters.resulute.the.blogServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,12 @@ public class WebConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "DELETE");
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
     }
 }
