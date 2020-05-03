fb0dce6072e0b6cbdbea22c84e377b49eeed308c
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-05-03 15:39:32 +0300

    change: testing

--- src/main/java/oysters/resulute/the/blogServer/SecSecurityConfig.java
+++ src/main/java/oysters/resulute/the/blogServer/SecSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!https")
 public class SecSecurityConfig
         extends WebSecurityConfigurerAdapter {
 
