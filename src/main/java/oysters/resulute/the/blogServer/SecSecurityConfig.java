b87e3a062a47d2e0575fc86d99c86dd00f24c471
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-05-03 15:39:03 +0300

    change: testing

---
+++ src/main/java/oysters/resulute/the/blogServer/SecSecurityConfig.java
@@ -0,0 +1,51 @@
+package oysters.resulute.the.blogServer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig
+        extends WebSecurityConfigurerAdapter {
+
+    //...
+
+    @Override
+    protected void configure(final HttpSecurity http)
+            throws Exception {
+        http.csrf().disable().authorizeRequests()
+                //...
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/index*", "/static/**", "/*.js", "/*.json", "/*.ico")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/index.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/homepage.html",true)
+                .failureUrl("/index.html?error=true");
+        //...
+    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("admin")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/oysters/resulute/the/blogServer/WebConfig.java
+++ src/main/java/oysters/resulute/the/blogServer/WebConfig.java
@@ -1,17 +1,21 @@
 package oysters.resulute.the.blogServer;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer
+public class WebConfig extends WebMvcConfigurerAdapter
 {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "DELETE");
+    }
+    @Override
+    public void addResourceHandlers(
+            ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
