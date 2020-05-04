5e0a294d4bdd857fdd60d6fddbfec01d6a661bd5
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-05-04 15:12:47 +0300

    add: missing files

---
+++ src/main/resources/static/precache-manifest.561f27f6f121224c278d4ade53e35f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae221058470de431da598a2452e3ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8702b88da33bd16d6b5d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b3812cc2e28b30326eb",
+    "url": "/static/js/2.d2fb59eb.chunk.js"
+  },
+  {
+    "revision": "1a5b0426ccdd3fae8bf397f36913f6f8",
+    "url": "/static/js/2.d2fb59eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8702b88da33bd16d6b5d",
+    "url": "/static/js/main.0573235b.chunk.js"
+  },
+  {
+    "revision": "f9fbf74a3dc11fb865b8",
+    "url": "/static/js/runtime-main.f6962a34.js"
+  }
+]);
